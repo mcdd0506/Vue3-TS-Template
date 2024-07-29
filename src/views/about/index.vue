f429587de6eb188ce27c7537e241ca0555a1762c
mcdd0506 <mcdd0506@gmail.com>
2024-07-29 22:49:53 +0800

    feat: element plus(auto import) demo

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>ABOUT</h1>
+  {{ num }}
+  <el-button :plain="true" @click="open">Show message</el-button>
+  <el-button :plain="true" @click="openVn">VNode</el-button>
 </template>
+
+<script lang="ts" setup>
+const open = () => {
+  ElMessage('This is a message.')
+}
+const num = ref(20)
+const openVn = () => {
+  ElMessage({
+    message: h('p', { style: 'line-height: 1; font-size: 14px' }, [
+      h('span', null, 'Message can be '),
+      h('i', { style: 'color: teal' }, 'VNode')
+    ])
+  })
+}
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,55 @@
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { fileURLToPath, URL } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
+import { createStyleImportPlugin, ElementPlusResolve } from 'vite-plugin-style-import'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver() // Auto import icon components
+      ],
+      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
+      dts: './types/auto-imports.d.ts',
+      // eslint 报错解决：'ref' is not defined
+      eslintrc: {
+        // 默认 false, true 启用生成。生成一次就可以，避免每次工程启动都生成，一旦生成配置文件之后，最好把 enable 关掉，即改成 false。结合文档 1
+        enabled: false
+        // 否则这个文件每次会在重新加载的时候重新生成，这会导致 eslint 有时会找不到这个文件。当需要更新配置文件的时候，再重新打开
+        // filepath: './.eslintrc-auto-import.json', // 默认就是 ./.eslintrc-auto-import.json
+        // globalsPropValue: true // 默认 true
+      }
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver()
+        // Auto register icon components
+        // 自动注册图标组件
+      ],
+      dts: './types/components.d.ts'
+    }),
+    createStyleImportPlugin({
+      resolves: [ElementPlusResolve()],
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          resolveStyle: (name: string) => {
+            return `element-plus/theme-chalk/${name}.css`
+          }
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
